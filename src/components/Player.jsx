5041665c9914fbdb8b28fb6dd6fc2f84d669cfd5
Marko Djordjevic <djomarko97@gmail.com>
2026-02-17 17:21:38 +0100

    feat(): Add functionality for the player block

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,45 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+
+  let btnCaption = "Edit";
+
+  function handleClick() {
+    // if (isEditing) {
+    //   setIsEditing(false);
+    // } else setIsEditing(true);
+
+    // 2nd way
+    // setIsEditing(!isEditing);
+
+    setIsEditing((editing) => (editing = !isEditing));
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  if (isEditing) btnCaption = "Save";
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        )}
+
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleClick}>{btnCaption}</button>
     </li>
   );
 }
