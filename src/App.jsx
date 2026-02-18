aaace79a61e0a40b735b51c96c695c2e78c1d327
Marko Djordjevic <djomarko97@gmail.com>
2026-02-18 15:47:50 +0100

    feat(): Add highlight class to the players and add lifting up the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((currentActivePlayer) =>
+      currentActivePlayer === "X" ? "O" : "X",
+    );
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard></GameBoard>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          symbol={activePlayer}
+        ></GameBoard>
       </div>
     </main>
   );
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ name, symbol, isActive }) {
   const [playerName, setPlayerName] = useState(name);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -24,7 +24,7 @@ export default function Player({ name, symbol }) {
   if (isEditing) btnCaption = "Save";
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {!isEditing ? (
           <span className="player-name">{playerName}</span>
